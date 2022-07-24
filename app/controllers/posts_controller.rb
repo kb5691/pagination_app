a8772ae72dcf0e2d913ab7fc92d7ccc0900a2851
oshiro <kaito.o.0628@gmail.com>
2022-07-24 18:07:56 +0900

    投稿一覧ページのページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
+  # 1ページの表示数
+  PER_PAGE = 10
+
   def index
-    @posts = Post.order(id: :desc)
+    @posts = Post.order(id: :desc).page(params[:page]).per(PER_PAGE)
   end
 
   def create
